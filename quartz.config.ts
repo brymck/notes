7ea3dca96253074e5275ec4bd7f34d5a31145c85
Bryan McKelvey <bryan.mckelvey@gmail.com>
2025-04-28 22:18:22 +0900

    Improve SCSS style

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Bryan's Notes",
+    pageTitle: "Marginalia",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "brymck.github.io/notes",
+    baseUrl: "brymck.github.io",
     ignorePatterns: [
       "private",
       "templates",
